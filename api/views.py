71f26591da3bd3dffd9c9b89ae24bba4d201c118
Martin Žůrek <zurek.m@email.cz>
2023-02-09 20:15:08 +0100

    API

---
+++ api/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import JsonResponse, HttpRequest, HttpResponse
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions, viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.parsers import JSONParser
+from rest_framework import generics
+from rest_framework import mixins
+
+from viewer.models import Portfolio
+
+from api.serializers import PortfolioSerializer
+
+
+# Create your views here.
+class Portfolio(viewsets.ModelViewSet):
+    queryset = Portfolio.objects.all()
+    serializer_class = PortfolioSerializer
+    permission_classes = [permissions.IsAuthenticated]
